9f0b05cacf45b5684bce72df57ffee52fdf3aec7
HannaBodnarovska <bodnarovska2000@gmail.com>
2023-09-15 20:02:32 +0200

    plus profile

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Profile } from "components/Profile/Profile";
+
+import user from './user.json';
+import friend from './friends.json';
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        // height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+
+    </div>
+  );
+};
