870158eaf519f4131b705a73c733c7c9e99ae2f1
HannaBodnarovska <bodnarovska2000@gmail.com>
2023-09-17 12:52:17 +0200

    TransactionHistory

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Profile } from "components/Profile/Profile.jsx";
 import {Statistics} from "components/Statistics/Statistics.jsx";
 import { FriendList } from "components/Friends/Friends";
+import {TransactionHistory} from "components/TransactionHistory/TransactionHistory";
 
 import user from './user.json';
 import data from './data.json';
 import friends from './friends.json';
+import transactions from './transactions.json'; 
 
 export const App = () => {
   return (
@@ -32,6 +34,8 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory transactions={transactions} />
 
     </div>
   );
