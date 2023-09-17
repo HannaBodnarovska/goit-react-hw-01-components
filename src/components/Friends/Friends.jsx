672a70d5ad0870d7839c402591ba83d60af9b2e1
HannaBodnarovska <bodnarovska2000@gmail.com>
2023-09-17 13:31:27 +0200

    the end

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -5,7 +5,7 @@ export const FriendList = ({ friends }) => {
         <ul className={css.friendList}>
             {friends.map(friend => (
                 <li className={css.item} key={friend.id}>
-                    <span className={css.status}>{friend.isOnline}</span>
+                    <span className={friend.isOnline ? css.online : css.offline}></span>
                     <img className={css.avatar} src={friend.avatar} alt={friend.name} width="48" />
                     <p className={css.name}>{friend.name}</p>
                 </li>
